62c2077b30fda2937cdbd9753329a1794116c645
Alexander Danilov <modos189@protonmail.com>
2019-10-21 22:25:05 +0300

    Add anchors for plugins and categories (issue #7)

--- static/js/user.js
+++ static/js/user.js
@@ -1,17 +1,27 @@
+function scrollToElement(element) {
+    if (!element) return;
+
+    let to = element.getBoundingClientRect().top;
+    let from = window.pageYOffset;
+
+    window.scrollTo(0, from + to | 0);
+}
+
 document.addEventListener('DOMContentLoaded', function() {
-    var options = {};
-    var elems = document.querySelectorAll('.sidenav');
-    var Sidenav_instances = M.Sidenav.init(elems, options);
+    let sidenav_elems = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav_elems, {});
 
-    var Carousel_instance = M.Carousel.init(document.querySelectorAll('.carousel'), {indicators: true});
+    M.Carousel.init(document.querySelectorAll('.carousel'), {indicators: true});
 
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems, {});
+    let collapsible_elems = document.querySelectorAll('.collapsible');
+    M.Collapsible.init(collapsible_elems, {});
+
+    if(window.location.hash) {
+        // Waiting for tile sorting by masonry library and anchor position correction.
+        // Several masonry instances are running, making it difficult to determine if all the work has been completed.
+        setTimeout(function(){
+            let target_element = document.getElementById(window.location.hash.split('#')[1]);
+            scrollToElement(target_element);
+        }, 1000);
+    }
 });
-
-// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
-// var collapsibleElem = document.querySelector('.collapsible');
-// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
-
-
-
