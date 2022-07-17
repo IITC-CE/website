20c1833c0f36e5ba1e25ddebd8129202321b805c
modos189 <git@modos189.ru>
2022-07-17 19:04:03 +0300

    Generation index.html with list of files

--- update.py
+++ update.py
@@ -9,6 +9,8 @@ import os
 import urllib.parse
 import hashlib
 import shutil
+import glob
+from datetime import datetime
 from bs4 import BeautifulSoup
 
 
@@ -99,11 +101,84 @@ def copy_last_build_from_archive():
     for release_type in ["release", "beta"]:
         build_path = f"static/build/{release_type}"
         builds_archive_path = f"static/build/{release_type}_archive"
-        last_build = sorted(os.listdir(builds_archive_path))[-1]
+
+        archives = [f for f in os.listdir(builds_archive_path) if os.path.isdir(os.path.join(builds_archive_path, f))]
+        last_build = sorted(archives)[-1]
 
         if os.path.exists(build_path):
             shutil.rmtree(build_path)
         shutil.copytree(f"{builds_archive_path}/{last_build}", build_path)
+
+
+def get_files_tree():
+    root = "static/"
+    tree = {}
+    for file_path in glob.iglob('static/**', recursive=True):
+        file_path = file_path.replace(root, "")
+        file_dir = "/".join(file_path.split("/")[:-1])
+        if file_dir not in tree:
+            tree[file_dir] = []
+        file_name = file_path.split("/")[-1]
+        tree[file_dir].append(file_name)
+    if "" in tree:
+        del tree[""]
+    return tree
+
+
+def sizeof_fmt(num, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f} {unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f} Yi{suffix}"
+
+
+def recursive_generate_index_pages():
+    tree = get_files_tree()
+
+    for file_dir in tree:
+        file_dir_split = file_dir.split("/")
+
+        marker_path_menu = []
+        marker_files = []
+        for i, split_dir in enumerate(file_dir_split):
+            marker_path_menu.append(["../"*(len(file_dir_split)-i-1), split_dir])
+
+        for file in tree[file_dir]:
+            if file == "index.html":
+                continue
+
+            file_path = f"{file_dir}/{file}"
+            static_file_path = "static/"+file_path
+            file_is_dir = os.path.isdir(static_file_path)
+            file_size = os.path.getsize(static_file_path) if not file_is_dir else "-1"
+            file_size_fmt = sizeof_fmt(file_size) if not file_is_dir else "&mdash;"
+            modified = os.path.getmtime(static_file_path)
+            file_modified = datetime.fromtimestamp(modified).strftime("%Y-%m-%dT%H:%M:%SZ")
+            file_modified_fmt = datetime.fromtimestamp(modified).strftime("%Y-%m-%d %H:%M:%S")
+
+            marker_files.append({
+                "is_dir": file_is_dir,
+                "name": file,
+                "path": f"./{file}",
+                "size": file_size,
+                "size_fmt": file_size_fmt,
+                "modified": file_modified,
+                "modified_fmt": file_modified_fmt
+            })
+
+        markers = {
+            "path": "/"+file_dir,
+            "path_menu": marker_path_menu,
+            "files": marker_files,
+            "count_dirs": sum(x['is_dir'] is True for x in marker_files),
+            "count_files": sum(x['is_dir'] is False for x in marker_files)
+        }
+        template = env.get_template("__folder_index__.html")
+        html = template.render(markers)
+        path = f"static/{file_dir}/index.html"
+        with open(path, "w") as fh:
+            fh.write(html)
 
 
 if __name__ == '__main__':
@@ -114,6 +189,7 @@ if __name__ == '__main__':
     env.filters['md5sum'] = file_add_md5sum
 
     copy_last_build_from_archive()
+    recursive_generate_index_pages()
 
     files = os.listdir("template")
     files = filter(lambda x: x.endswith(".html"), files)
