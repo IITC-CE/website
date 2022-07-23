8d9792f3e1b11bf6e558f1cfab2ceab5e6373c20
IITC-CE <carles3@pina.cat>
2022-07-23 00:35:53 +0000

    Update from https://github.com/IITC-CE/website/commit/a696bf15ff7bda2b87bf89b5cec2fd1f566d0d8d

---
+++ build/artifact/PR495/plugins/privacy-view.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Privacy view on Intel
+// @category       Misc
+// @version        1.1.0.20220723.003309
+// @description    Hide info from intel which shouldn't leak to players of the other faction.
+// @id             privacy-view
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR495/plugins/privacy-view.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR495/plugins/privacy-view.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
