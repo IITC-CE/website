c8a8982b1c660a81d10c046b5ae43c5553b29908
IITC-CE <carles3@pina.cat>
2022-07-25 21:41:28 +0000

    Update from https://github.com/IITC-CE/website/commit/6462e11d81311fbc4ef80a7e53f2be5f36367320

---
+++ build/artifact/PR576/plugins/basemap-openstreetmap.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: OpenStreetMap.org map
+// @category       Map Tiles
+// @version        0.1.1.20220725.213830
+// @description    Add the native OpenStreetMap.org map tiles as an optional layer.
+// @id             basemap-openstreetmap
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR576/plugins/basemap-openstreetmap.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR576/plugins/basemap-openstreetmap.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR576/plugins/reso-energy-pct.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         xelio
+// @name           IITC plugin: Reso energy % in portal details
+// @category       Portal Info
+// @version        0.1.2.20220725.213830
+// @description    Show resonator energy percentage on resonator energy bar in portal details panel.
+// @id             reso-energy-pct
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR576/plugins/reso-energy-pct.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR576/plugins/reso-energy-pct.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
