5e7c8848810297822fb2715cbb173479825189c0
IITC-CE <carles3@pina.cat>
2022-08-01 17:21:49 +0000

    Update from https://github.com/IITC-CE/website/commit/0893d364f0940fbcff443eb5ce475db4b9bbde37

---
+++ build/artifact/PR447/plugins/basemap-gaode.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Gaode (高德地图) / AutoNavi map
+// @category       Map Tiles
+// @version        0.1.0.20220801.171852
+// @description    Map layers from AutoNavi / Gaode (高德地图)
+// @id             basemap-gaode
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR447/plugins/basemap-gaode.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR447/plugins/basemap-gaode.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR447/plugins/zaprange.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         ZasoGD
+// @name           IITC plugin: Zaprange
+// @category       Layer
+// @version        0.1.5.20220801.171852
+// @description    Shows the maximum range of attack by the portals.
+// @id             zaprange
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR447/plugins/zaprange.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR447/plugins/zaprange.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
