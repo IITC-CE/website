343234e4b7d4db30daeb8667ba648bd3f7059fa0
IITC-CE <carles3@pina.cat>
2022-08-01 15:11:41 +0000

    Update from https://github.com/IITC-CE/website/commit/719628d6dac4e1b33926b1b9034ea38357ae7bab

---
+++ build/artifact/PR583/plugins/portal-level-numbers.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         rongou
+// @name           IITC plugin: Portal Level Numbers
+// @category       Layer
+// @version        0.2.1.20220801.150914
+// @description    Show portal level numbers on map.
+// @id             portal-level-numbers
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR583/plugins/portal-level-numbers.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR583/plugins/portal-level-numbers.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
