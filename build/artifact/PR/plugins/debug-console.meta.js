8f598eae221961d25c29f0b79d611409f6868792
IITC-CE <carles3@pina.cat>
2022-08-09 15:25:02 +0000

    Update from https://github.com/IITC-CE/website/commit/527887bc2a805593939d092399dc3590cb8c5c01

---
+++ build/artifact/PR/plugins/debug-console.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jaiperdu
+// @name           IITC plugin: Debug console tab
+// @category       Debug
+// @version        0.1.0.20220809.152125
+// @description    Add a debug console tab
+// @id             debug-console
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR585/plugins/debug-console.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR585/plugins/debug-console.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
