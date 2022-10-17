758f7dfec6e9c7572b9b4ca40723206c8b77ddcc
IITC-CE <carles3@pina.cat>
2022-10-17 16:26:57 +0000

    Update from https://github.com/IITC-CE/website/commit/8cf21cf9f9c610c5179acc088f30aff5c7112f50

--- build/artifact/PR591/plugins/pan-control.user.js
+++ build/artifact/PR591/plugins/pan-control.user.js
@@ -2,7 +2,7 @@
 // @author         fragger
 // @name           IITC plugin: Pan control
 // @category       Controls
-// @version        0.2.2.20221017.162131
+// @version        0.2.2.20221017.162337
 // @description    Show a panning control on the map.
 // @id             pan-control
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -19,7 +19,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2022-10-17-162131';
+plugin_info.dateTimeVersion = '2022-10-17-162337';
 plugin_info.pluginId = 'pan-control';
 //END PLUGIN AUTHORS NOTE
 
--- build/artifact/PR591/plugins/scoreboard.meta.js
+++ build/artifact/PR591/plugins/scoreboard.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author         Costaspap
 // @name           IITC plugin: Localized scoreboard
-// @version        0.3.1.20221017.162131
+// @version        0.3.1.20221017.162337
 // @category       Info
 // @description    Display a scoreboard about all visible portals with statistics about both teams,like average portal level,link & field counts etc.
 // @id             scoreboard
