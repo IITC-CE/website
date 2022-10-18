62081491fa8bf48ebccbc2c7b782fc5ae82f96e5
IITC-CE <carles3@pina.cat>
2022-10-18 12:22:18 +0000

    Update from https://github.com/IITC-CE/website/commit/5b6709c400457d79059a9d9203e13b43e2f37e50

---
+++ build/artifact/PR593/plugins/regions.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Ingress scoring regions
+// @category       Layer
+// @version        0.2.1.20221018.121704
+// @description    Show the regional scoring cells grid on the map
+// @id             regions
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR593/plugins/regions.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR593/plugins/regions.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
