73f336288e0c019168acfc77a2806cdb7c89477e
IITC-CE <carles3@pina.cat>
2022-10-30 22:51:40 +0000

    Update from https://github.com/IITC-CE/website/commit/6cbf4a9fcc9742a321f09d5be6c4752f602f76a6

--- build/artifact/PR594/plugins/basemap-yandex.meta.js
+++ build/artifact/PR594/plugins/basemap-yandex.meta.js
@@ -2,7 +2,7 @@
 // @author         johnd0e
 // @name           IITC plugin: Yandex maps
 // @category       Map Tiles
-// @version        0.3.0.20221030.223353
+// @version        0.3.0.20221030.224829
 // @description    Add Yandex.com (Russian/Русский) map layers
 // @id             basemap-yandex
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR594/plugins/fly-links.user.js
+++ build/artifact/PR594/plugins/fly-links.user.js
@@ -2,7 +2,7 @@
 // @author         Fly33
 // @name           IITC plugin: Fly Links
 // @category       Draw
-// @version        0.5.0.20221030.223353
+// @version        0.5.0.20221030.224829
 // @description    Calculate how to link the portals to create the largest tidy set of nested fields. Enable from the layer chooser.
 // @id             fly-links
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -19,7 +19,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2022-10-30-223353';
+plugin_info.dateTimeVersion = '2022-10-30-224829';
 plugin_info.pluginId = 'fly-links';
 //END PLUGIN AUTHORS NOTE
 
