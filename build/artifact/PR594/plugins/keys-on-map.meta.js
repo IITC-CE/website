1ce296f0bd078bcdd724a56e69af183fb92453c2
IITC-CE <carles3@pina.cat>
2022-10-30 22:30:38 +0000

    Update from https://github.com/IITC-CE/website/commit/a0f4826b4a16c01c5cfd3611ede5c8fcfddf5c7d

---
+++ build/artifact/PR594/plugins/keys-on-map.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         xelio
+// @name           IITC plugin: Keys on map
+// @category       Layer
+// @version        0.3.1.20221030.222739
+// @description    Show the manually entered key counts from the 'keys' plugin on the map.
+// @id             keys-on-map
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR594/plugins/keys-on-map.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR594/plugins/keys-on-map.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
