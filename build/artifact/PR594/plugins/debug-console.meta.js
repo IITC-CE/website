67993c060f054b280e70c800f3d16eed1a95c69e
IITC-CE <carles3@pina.cat>
2022-10-30 22:57:21 +0000

    Update from https://github.com/IITC-CE/website/commit/cb64430b08c09d925bb8eadb5e052b5b6d26080a

--- build/artifact/PR594/plugins/debug-console.meta.js
+++ build/artifact/PR594/plugins/debug-console.meta.js
@@ -2,7 +2,7 @@
 // @author         jaiperdu
 // @name           IITC plugin: Debug console tab
 // @category       Debug
-// @version        0.1.0.20221030.224829
+// @version        0.1.0.20221030.225425
 // @description    Add a debug console tab
 // @id             debug-console
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR594/plugins/highlight-portals-my-level.user.js
+++ build/artifact/PR594/plugins/highlight-portals-my-level.user.js
@@ -2,7 +2,7 @@
 // @author         vita10gy
 // @name           IITC plugin: Highlight portals by my level
 // @category       Highlighter
-// @version        0.2.0.20221030.224829
+// @version        0.2.0.20221030.225425
 // @description    Use the portal fill color to denote if the portal is either at and above, or at and below your level.
 // @id             highlight-portals-my-level
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -19,7 +19,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2022-10-30-224829';
+plugin_info.dateTimeVersion = '2022-10-30-225425';
 plugin_info.pluginId = 'highlight-portals-my-level';
 //END PLUGIN AUTHORS NOTE
 
