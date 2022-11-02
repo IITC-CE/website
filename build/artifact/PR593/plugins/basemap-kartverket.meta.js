21ece604da841ce3fd97e97ad05ed2a2ee0e4772
IITC-CE <carles3@pina.cat>
2022-11-02 10:18:22 +0000

    Update from https://github.com/IITC-CE/website/commit/f0d2a9a33e9e3f18c218bb287406aee39e53e9bc

--- build/artifact/PR593/plugins/basemap-kartverket.meta.js
+++ build/artifact/PR593/plugins/basemap-kartverket.meta.js
@@ -2,7 +2,7 @@
 // @author         johnd0e
 // @name           IITC plugin: Kartverket.no maps (Norway)
 // @category       Map Tiles
-// @version        0.2.1.20221102.101238
+// @version        0.2.1.20221102.101435
 // @description    Add Kartverket.no map layers.
 // @id             basemap-kartverket
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR593/plugins/highlight-level-color.user.js
+++ build/artifact/PR593/plugins/highlight-level-color.user.js
@@ -2,7 +2,7 @@
 // @author         vita10gy
 // @name           IITC plugin: Highlight portals by level color
 // @category       Highlighter
-// @version        0.2.0.20221102.101238
+// @version        0.2.0.20221102.101435
 // @description    Use the portal fill color to denote the portal level by using the game level colors.
 // @id             highlight-level-color
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -19,7 +19,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2022-11-02-101238';
+plugin_info.dateTimeVersion = '2022-11-02-101435';
 plugin_info.pluginId = 'highlight-level-color';
 //END PLUGIN AUTHORS NOTE
 
