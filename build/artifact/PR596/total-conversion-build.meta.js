2dac6f6c34a5d70d17822c559be36ccb494cea10
IITC-CE <carles3@pina.cat>
2022-11-03 09:03:42 +0000

    Update from https://github.com/IITC-CE/website/commit/5db7e7144168cb6abf86e4fc8bf726fa3dea1693

---
+++ build/artifact/PR596/total-conversion-build.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC: Ingress intel map total conversion
+// @version        0.33.0.20221103.085821
+// @description    Total conversion for the ingress intel map.
+// @run-at         document-end
+// @id             total-conversion-build
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR596/total-conversion-build.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR596/total-conversion-build.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
