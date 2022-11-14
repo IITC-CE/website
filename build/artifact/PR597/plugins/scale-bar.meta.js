a8a904aeb757dabd4b3db6b000afb1e3425258aa
IITC-CE <carles3@pina.cat>
2022-11-14 17:31:19 +0000

    Update from https://github.com/IITC-CE/website/commit/f6c9bdc5a36d49d3ce1666f13f32f81997141a94

--- build/artifact/PR597/plugins/scale-bar.meta.js
+++ build/artifact/PR597/plugins/scale-bar.meta.js
@@ -2,7 +2,7 @@
 // @author         breunigs
 // @name           IITC plugin: Scale bar
 // @category       Controls
-// @version        0.1.1.20221114.172023
+// @version        0.1.1.20221114.172802
 // @description    Show scale bar on the map.
 // @id             scale-bar
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
