d6486d3dea7b05e8fac03be196b392786045d3c2
IITC-CE <carles3@pina.cat>
2022-11-14 23:54:40 +0000

    Update from https://github.com/IITC-CE/website/commit/f34ce2c089362d6b9e5acaab639a06116e008b2d

--- build/artifact/PR597/plugins/done-links.user.js
+++ build/artifact/PR597/plugins/done-links.user.js
@@ -2,7 +2,7 @@
 // @author         jonatkins
 // @name           IITC plugin: Done links
 // @category       Draw
-// @version        0.1.1.20221114.233901
+// @version        0.1.1.20221114.235107
 // @description    A companion to the Cross Links plugin. Highlights any links that match existing draw-tools line/polygon edges
 // @id             done-links
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -20,7 +20,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2022-11-14-233901';
+plugin_info.dateTimeVersion = '2022-11-14-235107';
 plugin_info.pluginId = 'done-links';
 //END PLUGIN AUTHORS NOTE
 
--- build/artifact/PR597/plugins/linked-portals-show.user.js
+++ build/artifact/PR597/plugins/linked-portals-show.user.js
@@ -2,7 +2,7 @@
 // @author         fstopienski
 // @name           IITC plugin: Linked portals
 // @category       Portal Info
-// @version        0.4.0.20221114.233901
+// @version        0.4.0.20221114.235107
 // @description    Try to show the linked portals (image, name and link direction) in portal detail view and jump to linked portal on click.  Some details may not be available if the linked portal is not in the current view.
 // @id             linked-portals-show
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -20,7 +20,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2022-11-14-233901';
+plugin_info.dateTimeVersion = '2022-11-14-235107';
 plugin_info.pluginId = 'linked-portals-show';
 //END PLUGIN AUTHORS NOTE
 
