4a0e02eb6598592183e45ddc61f851bacd8b6b83
IITC-CE <carles3@pina.cat>
2022-11-16 11:42:21 +0000

    Update from https://github.com/IITC-CE/website/commit/8726e6d324d684ef9f460f3ae274c2b57a10bef8

---
+++ build/artifact/PR577/plugins/debug-raw-portal-data.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Debug: Raw portal JSON data
+// @category       Portal Info
+// @version        0.2.4.20221116.113900
+// @description    Developer debugging aid: Add a link to the portal details to show the raw data of a portal.
+// @id             debug-raw-portal-data
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR577/plugins/debug-raw-portal-data.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR577/plugins/debug-raw-portal-data.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @grant          none
+// ==/UserScript==
