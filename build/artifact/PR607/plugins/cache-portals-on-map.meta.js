4e124846d2897375ca10a8666393a86e20828676
IITC-CE <carles3@pina.cat>
2022-12-11 18:03:55 +0000

    Update from https://github.com/IITC-CE/website/commit/f9edde3d06088fdcf92d0d9f5a49155eef620957

---
+++ build/artifact/PR607/plugins/cache-portals-on-map.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Cache viewed portals on map
+// @category       Cache
+// @version        0.1.0.20221211.180030
+// @description    Cache the details of recently viewed portals and use this to populate the map when possible
+// @id             cache-portals-on-map
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR607/plugins/cache-portals-on-map.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR607/plugins/cache-portals-on-map.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR607/total-conversion-build.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC: Ingress intel map total conversion
+// @version        0.34.0.20221211.180030
+// @description    Total conversion for the ingress intel map.
+// @run-at         document-end
+// @id             total-conversion-build
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR607/total-conversion-build.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR607/total-conversion-build.user.js
+// @match          https://intel.ingress.com/*
+// @grant          none
+// ==/UserScript==
