86f1226ad4f6fbca6ba05838777b48496bf3e284
IITC-CE <carles3@pina.cat>
2022-12-12 10:44:20 +0000

    Update from https://github.com/IITC-CE/website/commit/089d393badef20a0812eeb26a3be17beaaf60b33

---
+++ build/artifact/PR608/plugins/ornament-icons.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @author         johtata
+// @name           IITC plugin: Ornament icons basic
+// @category       Layer
+// @version        0.1.0.20221212.104038
+// @description    Add own icons and names for ornaments
+// @id             ornament-icons
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR608/plugins/ornament-icons.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR608/plugins/ornament-icons.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @grant          none
+// ==/UserScript==
