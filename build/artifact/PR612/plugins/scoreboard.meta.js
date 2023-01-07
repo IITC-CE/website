78bbc911194842819c0c000e0270bfb66062ee34
IITC-CE <carles3@pina.cat>
2023-01-07 18:02:54 +0000

    Update from https://github.com/IITC-CE/website/commit/cdaa788548a15ac56b78c08423e6990968fb70de

--- build/artifact/PR612/plugins/scoreboard.meta.js
+++ build/artifact/PR612/plugins/scoreboard.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author         Costaspap
 // @name           IITC plugin: Localized scoreboard
-// @version        0.3.2.20230107.175629
+// @version        0.3.2.20230107.175901
 // @category       Info
 // @description    Display a scoreboard about all visible portals with statistics about both teams,like average portal level,link & field counts etc.
 // @id             scoreboard
--- build/artifact/PR612/total-conversion-build.meta.js
+++ build/artifact/PR612/total-conversion-build.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author         jonatkins
 // @name           IITC: Ingress intel map total conversion
-// @version        0.34.1.20230107.175629
+// @version        0.34.1.20230107.175901
 // @description    Total conversion for the ingress intel map.
 // @run-at         document-end
 // @id             total-conversion-build
