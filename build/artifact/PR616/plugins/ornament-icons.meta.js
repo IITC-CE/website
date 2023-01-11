b4ff491ef6a0643863c015b490ba87f3338c6ca4
IITC-CE <carles3@pina.cat>
2023-01-11 14:34:19 +0000

    Update from https://github.com/IITC-CE/website/commit/294785e47d7213f9747c2accffe1e99c4b2a9a10

--- build/artifact/PR616/plugins/ornament-icons.meta.js
+++ build/artifact/PR616/plugins/ornament-icons.meta.js
@@ -2,7 +2,7 @@
 // @author         johtata
 // @name           IITC plugin: Ornament icons basic
 // @category       Layer
-// @version        0.1.0.20230111.132706
+// @version        0.1.0.20230111.142958
 // @description    Add own icons and names for ornaments
 // @id             ornament-icons
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR616/plugins/portal-level-numbers.meta.js
+++ build/artifact/PR616/plugins/portal-level-numbers.meta.js
@@ -2,7 +2,7 @@
 // @author         rongou
 // @name           IITC plugin: Portal Level Numbers
 // @category       Layer
-// @version        0.2.1.20230111.132706
+// @version        0.2.1.20230111.142958
 // @description    Show portal level numbers on map.
 // @id             portal-level-numbers
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
