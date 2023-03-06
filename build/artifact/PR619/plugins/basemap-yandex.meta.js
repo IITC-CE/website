26fd4f92ffb2892271429071e68bf35304f82187
IITC-CE <carles3@pina.cat>
2023-03-06 08:27:57 +0000

    Update from https://github.com/IITC-CE/website/commit/0fbf0f4fd357ae9a9766c35b30b0bd483b2ab9c7

---
+++ build/artifact/PR619/plugins/basemap-yandex.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Yandex maps
+// @category       Map Tiles
+// @version        0.3.1.20230306.082251
+// @description    Add Yandex.com (Russian/Русский) map layers
+// @id             basemap-yandex
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR619/plugins/basemap-yandex.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR619/plugins/basemap-yandex.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR619/plugins/zaprange.meta.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @author         ZasoGD
+// @name           IITC plugin: Zaprange
+// @category       Layer
+// @version        0.1.5.20230306.082251
+// @description    Shows the maximum range of attack by the portals.
+// @id             zaprange
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR619/plugins/zaprange.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR619/plugins/zaprange.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @grant          none
+// ==/UserScript==
