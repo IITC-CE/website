9ad8fdd023d454e7b84bc5b8e5096a1f614e33cb
IITC-CE <carles3@pina.cat>
2023-03-10 22:38:40 +0000

    Update from https://github.com/IITC-CE/website/commit/c921b6f77eccfefdcc7d90ee6af049d539b5c14f

--- build/artifact/PR621/plugins/basemap-stamen.user.js
+++ build/artifact/PR621/plugins/basemap-stamen.user.js
@@ -2,7 +2,7 @@
 // @author         jonatkins
 // @name           IITC plugin: Stamen.com map layers
 // @category       Map Tiles
-// @version        0.2.2.20230310.223109
+// @version        0.2.2.20230310.223447
 // @description    Add the 'Toner' and 'Watercolor' map layers from maps.stamen.com.
 // @id             basemap-stamen
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -20,7 +20,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2023-03-10-223109';
+plugin_info.dateTimeVersion = '2023-03-10-223447';
 plugin_info.pluginId = 'basemap-stamen';
 //END PLUGIN AUTHORS NOTE
 
--- build/artifact/PR621/plugins/portal-level-numbers.meta.js
+++ build/artifact/PR621/plugins/portal-level-numbers.meta.js
@@ -2,7 +2,7 @@
 // @author         rongou
 // @name           IITC plugin: Portal Level Numbers
 // @category       Layer
-// @version        0.2.1.20230310.223109
+// @version        0.2.1.20230310.223447
 // @description    Show portal level numbers on map.
 // @id             portal-level-numbers
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
