f730c6808d7f5cf76c5258c6ed9f53794b4e7452
IITC-CE <carles3@pina.cat>
2023-03-10 23:15:18 +0000

    Update from https://github.com/IITC-CE/website/commit/11108abe203509857d6676be480102d1089f6019

--- build/artifact/PR621/plugins/basemap-yandex.user.js
+++ build/artifact/PR621/plugins/basemap-yandex.user.js
@@ -2,7 +2,7 @@
 // @author         johnd0e
 // @name           IITC plugin: Yandex maps
 // @category       Map Tiles
-// @version        0.3.1.20230310.230831
+// @version        0.3.1.20230310.230959
 // @description    Add Yandex.com (Russian/Русский) map layers
 // @id             basemap-yandex
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -20,7 +20,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2023-03-10-230831';
+plugin_info.dateTimeVersion = '2023-03-10-230959';
 plugin_info.pluginId = 'basemap-yandex';
 //END PLUGIN AUTHORS NOTE
 
--- build/artifact/PR621/plugins/pan-control.meta.js
+++ build/artifact/PR621/plugins/pan-control.meta.js
@@ -2,7 +2,7 @@
 // @author         fragger
 // @name           IITC plugin: Pan control
 // @category       Controls
-// @version        0.2.2.20230310.230831
+// @version        0.2.2.20230310.230959
 // @description    Show a panning control on the map.
 // @id             pan-control
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
