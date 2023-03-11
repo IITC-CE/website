ebaf04328a903474a48b77e663faf0f4ba311534
IITC-CE <carles3@pina.cat>
2023-03-11 18:24:02 +0000

    Update from https://github.com/IITC-CE/website/commit/85b95016e05ca950765bc8b09a0daed9c9418462

--- build/artifact/PR420/plugins/ornament-icons-extended.meta.js
+++ build/artifact/PR420/plugins/ornament-icons-extended.meta.js
@@ -2,7 +2,7 @@
 // @author         johtata
 // @name           IITC plugin: Ornament icons extended
 // @category       Layer
-// @version        0.1.0.20230311.180352
+// @version        0.1.0.20230311.181938
 // @description    Additonal icons and names for beacons
 // @id             ornament-icons-extended
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
