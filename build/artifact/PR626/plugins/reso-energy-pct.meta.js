227be49aa99056a36568151ef42e3a5f751265fe
IITC-CE <carles3@pina.cat>
2023-03-22 13:59:44 +0000

    Update from https://github.com/IITC-CE/website/commit/64647d48604cc42bc09801c2da09f65fc98d7ecf

---
+++ build/artifact/PR626/plugins/reso-energy-pct.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         xelio
+// @name           IITC plugin: Reso energy % in portal details
+// @category       Portal Info
+// @version        0.1.2.20230322.135512
+// @description    Show resonator energy percentage on resonator energy bar in portal details panel.
+// @id             reso-energy-pct
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR626/plugins/reso-energy-pct.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR626/plugins/reso-energy-pct.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/reso-energy-pct.png
+// @icon64         https://iitc.app/extras/plugin-icons/reso-energy-pct-64.png
+// @grant          none
+// ==/UserScript==
