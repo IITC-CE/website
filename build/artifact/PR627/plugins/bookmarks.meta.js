bfe8a4798c06ad0dc95c232022c8d3d891a16b83
IITC-CE <carles3@pina.cat>
2023-03-26 13:48:14 +0000

    Update from https://github.com/IITC-CE/website/commit/45006b4332d2db2e31daf6a7ea730d6265e1df96

---
+++ build/artifact/PR627/plugins/bookmarks.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         ZasoGD
+// @name           IITC plugin: Bookmarks for maps and portals
+// @category       Controls
+// @version        0.4.2.20230326.134411
+// @description    Save your favorite Maps and Portals and move the intel map with a click. Works with sync. Supports Multi-Project-Extension
+// @id             bookmarks
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR627/plugins/bookmarks.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR627/plugins/bookmarks.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/bookmarks.png
+// @icon64         https://iitc.app/extras/plugin-icons/bookmarks-64.png
+// @grant          none
+// ==/UserScript==
