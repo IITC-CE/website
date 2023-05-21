e35facf41ca16e003c58795902d9405c88c06be7
IITC-CE <carles3@pina.cat>
2023-05-21 13:06:33 +0000

    Update from https://github.com/IITC-CE/website/commit/31175dcd0f4500b4f6a53eea887f03889d2a468b

--- build/artifact/PR629/plugins/layer-count.meta.js
+++ build/artifact/PR629/plugins/layer-count.meta.js
@@ -2,7 +2,7 @@
 // @author         fkloft
 // @name           IITC plugin: Layer count
 // @category       Info
-// @version        0.2.2.20230521.125130
+// @version        0.2.2.20230521.130120
 // @description    Allow users to count nested fields
 // @id             layer-count
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR629/plugins/machina_tracker.user.meta.js
+++ build/artifact/PR629/plugins/machina_tracker.user.meta.js
@@ -2,7 +2,7 @@
 // @name           IITC plugin: IITC Plugin: Machina tracker
 // @author         McBen
 // @category       Layer
-// @version        1.0.0.20230521.125130
+// @version        1.0.0.20230521.130120
 // @description    Show locations of Machina activities
 // @id             machina_tracker.user
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
