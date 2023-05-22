e4084cfaa3faa02c42e7b2434cd2425309ca2a35
IITC-CE <carles3@pina.cat>
2023-05-22 18:51:04 +0000

    Update from https://github.com/IITC-CE/website/commit/abdd530b152351c3abd8ed0cc42cdb1f957dfe86

---
+++ build/artifact/PR633/plugins/link-show-direction.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Direction of links on map
+// @category       Tweaks
+// @version        0.2.1.20230522.184604
+// @description    Show the direction of links on the map by adding short dashes to the line at the origin portal.
+// @id             link-show-direction
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR633/plugins/link-show-direction.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR633/plugins/link-show-direction.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/link-show-direction.png
+// @icon64         https://iitc.app/extras/plugin-icons/link-show-direction-64.png
+// @grant          none
+// ==/UserScript==
