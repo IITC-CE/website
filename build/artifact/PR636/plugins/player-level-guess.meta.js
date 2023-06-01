cd68d813ca0fe31793a09e02f2ab3243868d96d3
IITC-CE <carles3@pina.cat>
2023-06-01 00:41:38 +0000

    Update from https://github.com/IITC-CE/website/commit/0eed47fd88f4d0500600fbaa186d3aeee26ea7c0

---
+++ build/artifact/PR636/plugins/player-level-guess.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         breunigs
+// @name           IITC plugin: Player level guess
+// @category       Info
+// @version        0.5.7.20230601.003636
+// @description    Try to determine player levels from the data available in the current view.
+// @id             player-level-guess
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR636/plugins/player-level-guess.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR636/plugins/player-level-guess.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/player-level-guess.png
+// @icon64         https://iitc.app/extras/plugin-icons/player-level-guess-64.png
+// @grant          none
+// ==/UserScript==
