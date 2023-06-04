01d8301b742f8bda80804ab18ce87d95a4076922
IITC-CE <carles3@pina.cat>
2023-06-04 00:28:00 +0000

    Update from https://github.com/IITC-CE/website/commit/75369b29bd8f3854adb6d410efdc6998a01a015e

---
+++ build/artifact/PR637/plugins/basemap-gaode.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Gaode (高德地图) / AutoNavi map
+// @category       Map Tiles
+// @version        0.1.1.20230604.002058
+// @description    Map layers from AutoNavi / Gaode (高德地图)
+// @id             basemap-gaode
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR637/plugins/basemap-gaode.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR637/plugins/basemap-gaode.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/basemap-gaode.png
+// @icon64         https://iitc.app/extras/plugin-icons/basemap-gaode-64.png
+// @grant          none
+// ==/UserScript==
