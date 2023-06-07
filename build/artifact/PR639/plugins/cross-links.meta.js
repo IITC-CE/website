5c8f2305de33bcbba46c5a65f5220cf86e22125f
IITC-CE <carles3@pina.cat>
2023-06-07 00:59:04 +0000

    Update from https://github.com/IITC-CE/website/commit/ab3cee6aab4b627e2f22483a9cd20e1b177bcdf1

--- build/artifact/PR639/plugins/cross-links.meta.js
+++ build/artifact/PR639/plugins/cross-links.meta.js
@@ -2,7 +2,7 @@
 // @author         mcben
 // @name           IITC plugin: Cross links
 // @category       Draw
-// @version        1.3.1.20230607.003014
+// @version        1.3.1.20230607.005406
 // @description    Checks for existing links that cross planned links. Requires draw-tools plugin.
 // @id             cross-links
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR639/total-conversion-build.meta.js
+++ build/artifact/PR639/total-conversion-build.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author         jonatkins
 // @name           IITC: Ingress intel map total conversion
-// @version        0.35.0.20230607.003014
+// @version        0.35.0.20230607.005406
 // @description    Total conversion for the ingress intel map.
 // @run-at         document-end
 // @id             total-conversion-build
