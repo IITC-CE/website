1677666a4a532673334db5c3b63905a066d4f56e
IITC-CE <carles3@pina.cat>
2023-06-14 07:16:12 +0000

    Update from https://github.com/IITC-CE/website/commit/6e0fc2f2a28234fefcc737b0f8ee5377ec5fcb0b

---
+++ build/artifact/PR640/plugins/portal-counts.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         yenky
+// @name           IITC plugin: Portal count
+// @category       Info
+// @version        0.2.2.20230614.071123
+// @description    Display a list of all localized portals by level and faction.
+// @id             portal-counts
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR640/plugins/portal-counts.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR640/plugins/portal-counts.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/portal-counts.png
+// @icon64         https://iitc.app/extras/plugin-icons/portal-counts-64.png
+// @grant          none
+// ==/UserScript==
