3dcca736aaf0f4cd2652b63a31e1ab3b96015c8b
IITC-CE <carles3@pina.cat>
2023-07-20 02:51:28 +0000

    Update from https://github.com/IITC-CE/website/commit/25326969614b98f1187e88b2199a097f09e5897c

--- build/artifact/PR649/plugins/basemap-yandex.meta.js
+++ build/artifact/PR649/plugins/basemap-yandex.meta.js
@@ -2,7 +2,7 @@
 // @author         johnd0e
 // @name           IITC plugin: Yandex maps
 // @category       Map Tiles
-// @version        0.3.1.20230720.023645
+// @version        0.3.1.20230720.024542
 // @description    Add Yandex.com (Russian/Русский) map layers
 // @id             basemap-yandex
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR649/plugins/player-level-guess.meta.js
+++ build/artifact/PR649/plugins/player-level-guess.meta.js
@@ -2,7 +2,7 @@
 // @author         breunigs
 // @name           IITC plugin: Player level guess
 // @category       Info
-// @version        0.5.7.20230720.023645
+// @version        0.5.7.20230720.024542
 // @description    Try to determine player levels from the data available in the current view.
 // @id             player-level-guess
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
