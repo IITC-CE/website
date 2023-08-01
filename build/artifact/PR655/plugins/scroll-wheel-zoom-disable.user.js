a5e22b3b8e027926c0f2ce3a6bf1ef2bc3caa784
IITC-CE <carles3@pina.cat>
2023-08-01 16:30:39 +0000

    Update from https://github.com/IITC-CE/website/commit/52d6a700884cf1dae521ecacfb431038b72383d2

---
+++ build/artifact/PR655/plugins/scroll-wheel-zoom-disable.user.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Disable mouse wheel zoom
+// @category       Tweaks
+// @version        0.1.0.20230801.162352
+// @description    Disable the use of mouse wheel to zoom. The map zoom controls or keyboard are still available.
+// @id             scroll-wheel-zoom-disable
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR655/plugins/scroll-wheel-zoom-disable.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR655/plugins/scroll-wheel-zoom-disable.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/scroll-wheel-zoom-disable.png
+// @icon64         https://iitc.app/extras/plugin-icons/scroll-wheel-zoom-disable-64.png
+// @grant          none
+// ==/UserScript==
+
+function wrapper(plugin_info) {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'test';
+plugin_info.dateTimeVersion = '2023-08-01-162352';
+plugin_info.pluginId = 'scroll-wheel-zoom-disable';
+//END PLUGIN AUTHORS NOTE
+
+
+// use own namespace for plugin
+window.plugin.scrollWheelZoomDisable = function() {};
+
+window.plugin.scrollWheelZoomDisable.setup = function() {
+
+  window.map.scrollWheelZoom.disable();
+
+};
+
+var setup =  window.plugin.scrollWheelZoomDisable.setup;
+
+setup.info = plugin_info; //add the script info data to the function as a property
+if (typeof changelog !== 'undefined') setup.info.changelog = changelog;
+if(!window.bootPlugins) window.bootPlugins = [];
+window.bootPlugins.push(setup);
+// if IITC has already booted, immediately run the 'setup' function
+if(window.iitcLoaded && typeof setup === 'function') setup();
+} // wrapper end
+// inject code into site context
+var script = document.createElement('script');
+var info = {};
+if (typeof GM_info !== 'undefined' && GM_info && GM_info.script) info.script = { version: GM_info.script.version, name: GM_info.script.name, description: GM_info.script.description };
+script.appendChild(document.createTextNode('('+ wrapper +')('+JSON.stringify(info)+');'));
+(document.body || document.head || document.documentElement).appendChild(script);
+
