8cb49d3411ca3f2399795e2f34a4a902cb3d6bda
IITC-CE <carles3@pina.cat>
2023-09-11 22:59:34 +0000

    Update from https://github.com/IITC-CE/website/commit/720fd565280c3602934310e75ebd1e57bcc9fe6a

--- build/artifact/PR673/plugins/cross-links.meta.js
+++ build/artifact/PR673/plugins/cross-links.meta.js
@@ -2,7 +2,7 @@
 // @author         mcben
 // @name           IITC plugin: Cross links
 // @category       Draw
-// @version        1.3.1.20230911.224635
+// @version        1.3.1.20230911.225032
 // @description    Checks for existing links that cross planned links. Requires draw-tools plugin.
 // @id             cross-links
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
