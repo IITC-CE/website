caade0180d94cce04d6bac7d00c0f343ce05c6e0
IITC-CE <carles3@pina.cat>
2023-09-13 20:18:19 +0000

    Update from https://github.com/IITC-CE/website/commit/8f7a4c86b8a8f7e320360d332f0a302b238af6d3

--- build/artifact/PR673/plugins/basemap-yandex.meta.js
+++ build/artifact/PR673/plugins/basemap-yandex.meta.js
@@ -2,7 +2,7 @@
 // @author         johnd0e
 // @name           IITC plugin: Yandex maps
 // @category       Map Tiles
-// @version        0.3.1.20230913.031536
+// @version        0.3.1.20230913.201238
 // @description    Add Yandex.com (Russian/Русский) map layers
 // @id             basemap-yandex
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
