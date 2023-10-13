edb0408dcfc8434371ec879c3b979f229eab8fc7
IITC-CE <carles3@pina.cat>
2023-10-13 16:46:13 +0000

    Update from https://github.com/IITC-CE/website/commit/302c826ea2b902b4be9e51aa00148ed3ef45d037

--- build/artifact/PR632/plugins/ap-stats.user.js
+++ build/artifact/PR632/plugins/ap-stats.user.js
@@ -2,7 +2,7 @@
 // @author         Hollow011
 // @name           IITC plugin: Available AP statistics
 // @category       Info
-// @version        0.4.2.20231013.163609
+// @version        0.4.2.20231013.163932
 // @description    Displays the per-team AP gains available in the current view.
 // @id             ap-stats
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -22,7 +22,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2023-10-13-163609';
+plugin_info.dateTimeVersion = '2023-10-13-163932';
 plugin_info.pluginId = 'ap-stats';
 //END PLUGIN AUTHORS NOTE
 
--- build/artifact/PR632/plugins/tidy-links.meta.js
+++ build/artifact/PR632/plugins/tidy-links.meta.js
@@ -2,7 +2,7 @@
 // @author         boombuler
 // @name           IITC plugin: Tidy Links
 // @category       Draw
-// @version        0.6.0.20231013.163609
+// @version        0.6.0.20231013.163932
 // @description    Calculate how to link the portals to create a reasonably tidy set of links/fields. Enable from the layer chooser. (former `Max Links`)
 // @id             tidy-links
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
