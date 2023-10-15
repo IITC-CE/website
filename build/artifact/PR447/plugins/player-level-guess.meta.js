ddf136a9fbd944bf40fe3fd5488b137cfefcd472
IITC-CE <carles3@pina.cat>
2023-10-15 09:28:24 +0000

    Update from https://github.com/IITC-CE/website/commit/1f5a62ebecaa9068f8c57c7482e389f86f00c793

--- build/artifact/PR447/plugins/player-level-guess.meta.js
+++ build/artifact/PR447/plugins/player-level-guess.meta.js
@@ -2,7 +2,7 @@
 // @author         breunigs
 // @name           IITC plugin: Player level guess
 // @category       Info
-// @version        0.5.7.20231014.173624
+// @version        0.5.7.20231015.092240
 // @description    Try to determine player levels from the data available in the current view.
 // @id             player-level-guess
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR447/plugins/portal-level-numbers.meta.js
+++ build/artifact/PR447/plugins/portal-level-numbers.meta.js
@@ -2,7 +2,7 @@
 // @author         rongou
 // @name           IITC plugin: Portal Level Numbers
 // @category       Layer
-// @version        0.2.1.20231014.173624
+// @version        0.2.1.20231015.092240
 // @description    Show portal level numbers on map.
 // @id             portal-level-numbers
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
