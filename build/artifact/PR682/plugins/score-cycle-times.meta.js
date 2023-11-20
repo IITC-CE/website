2918b6885069a9cdd3c38fe6628e92d2394674cf
IITC-CE <carles3@pina.cat>
2023-11-20 03:10:48 +0000

    Update from https://github.com/IITC-CE/website/commit/cb7689a862580becffcdf68275a388e306df5ed8

---
+++ build/artifact/PR682/plugins/score-cycle-times.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Scoring cycle / checkpoint times
+// @category       Info
+// @version        0.2.1.20231120.030525
+// @description    Show the times used for the septicycle and checkpoints for regional scoreboards.
+// @id             score-cycle-times
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR682/plugins/score-cycle-times.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR682/plugins/score-cycle-times.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/score-cycle-times.png
+// @icon64         https://iitc.app/extras/plugin-icons/score-cycle-times-64.png
+// @grant          none
+// ==/UserScript==
