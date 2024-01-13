996056b2fb7e0f8b7e26f4963cf97af45b667cb4
IITC-CE <carles3@pina.cat>
2024-01-13 10:37:45 +0000

    Update from https://github.com/IITC-CE/website/commit/452dfac60799e91b8695a945f7da2a648b564587

---
+++ build/artifact/PR686/plugins/sync.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         xelio
+// @name           IITC plugin: Sync
+// @category       Misc
+// @version        0.5.0.20240113.103229
+// @description    Sync data between clients via Google Drive API. Only syncs data from specific plugins (currently: Keys, Bookmarks, Uniques). Sign in via the 'Sync' link. Data is synchronized every 3 minutes.
+// @id             sync
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR686/plugins/sync.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR686/plugins/sync.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/sync.png
+// @icon64         https://iitc.app/extras/plugin-icons/sync-64.png
+// @grant          none
+// ==/UserScript==
