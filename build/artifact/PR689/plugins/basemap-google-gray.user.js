e879112d2c54a4bc97e42d8535bfceaa45e4cc85
IITC-CE <carles3@pina.cat>
2024-01-20 07:59:09 +0000

    Update from https://github.com/IITC-CE/website/commit/216edd8f8fceabd975e8f4a91114f24273a81dfa

--- build/artifact/PR689/plugins/basemap-google-gray.user.js
+++ build/artifact/PR689/plugins/basemap-google-gray.user.js
@@ -2,7 +2,7 @@
 // @author         jacob1123
 // @name           IITC plugin: Gray Google map
 // @category       Map Tiles
-// @version        0.1.5.20240120.074305
+// @version        0.1.5.20240120.075323
 // @description    Add a simplified gray Version of Google map tiles as an optional layer.
 // @id             basemap-google-gray
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -22,7 +22,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2024-01-20-074305';
+plugin_info.dateTimeVersion = '2024-01-20-075323';
 plugin_info.pluginId = 'basemap-google-gray';
 //END PLUGIN AUTHORS NOTE
 
--- build/artifact/PR689/plugins/machina-tracker.meta.js
+++ build/artifact/PR689/plugins/machina-tracker.meta.js
@@ -2,7 +2,7 @@
 // @name           IITC plugin: Machina tracker
 // @author         McBen
 // @category       Layer
-// @version        1.0.0.20240120.074305
+// @version        1.0.0.20240120.075323
 // @description    Show locations of Machina activities
 // @id             machina-tracker
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
