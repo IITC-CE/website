209ee802871f69a67f8e6662586a2b4ad1f62cf5
IITC-CE <carles3@pina.cat>
2024-01-23 06:46:24 +0000

    Update from https://github.com/IITC-CE/website/commit/0c0aaa3f651291c8b9845dca54f9b533ce4ccb8e

--- build/artifact/PR686/plugins/basemap-yandex.user.js
+++ build/artifact/PR686/plugins/basemap-yandex.user.js
@@ -2,7 +2,7 @@
 // @author         johnd0e
 // @name           IITC plugin: Yandex maps
 // @category       Map Tiles
-// @version        0.3.2.20240123.063705
+// @version        0.3.2.20240123.064126
 // @description    Add Yandex.com (Russian/Русский) map layers
 // @id             basemap-yandex
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -22,7 +22,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2024-01-23-063705';
+plugin_info.dateTimeVersion = '2024-01-23-064126';
 plugin_info.pluginId = 'basemap-yandex';
 //END PLUGIN AUTHORS NOTE
 
