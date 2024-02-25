9ca262a601ae800509d9aed98776fd22d5fbf6fe
IITC-CE <carles3@pina.cat>
2024-02-25 10:30:12 +0000

    Update from https://github.com/IITC-CE/website/commit/95a2c6186a27e22e909e85677b2c2c2901cc7432

---
+++ build/artifact/PR710/plugins/distance-to-portal.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Distance to portal
+// @category       Portal Info
+// @version        0.2.1.20240225.102513
+// @description    Allows your current location to be set manually, then shows the distance to the selected portal. Useful when managing portal keys.
+// @id             distance-to-portal
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR710/plugins/distance-to-portal.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR710/plugins/distance-to-portal.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/distance-to-portal.png
+// @icon64         https://iitc.app/extras/plugin-icons/distance-to-portal-64.png
+// @grant          none
+// ==/UserScript==
