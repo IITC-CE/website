0004c454293ab754a6baf3679f8619f239df251c
IITC-CE <carles3@pina.cat>
2024-03-12 10:44:05 +0000

    Update from https://github.com/IITC-CE/website/commit/8c20a2b93b27a4da977c0c1bcc705418f9e7d51d

---
+++ build/artifact/PR717/plugins/pan-control.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         fragger
+// @name           IITC plugin: Pan control
+// @category       Controls
+// @version        0.2.3.20240312.103857
+// @description    Show a panning control on the map.
+// @id             pan-control
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR717/plugins/pan-control.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR717/plugins/pan-control.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/pan-control.png
+// @icon64         https://iitc.app/extras/plugin-icons/pan-control-64.png
+// @grant          none
+// ==/UserScript==
