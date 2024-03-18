4cfe6446df99bdadc85b736f2ce2aa692fe2c926
IITC-CE <carles3@pina.cat>
2024-03-18 18:49:34 +0000

    Update from https://github.com/IITC-CE/website/commit/06e70f5de68ca057b5f85d82d9acb3c6fbadc8a5

--- build/artifact/PR711/plugins/draw-tools.meta.js
+++ build/artifact/PR711/plugins/draw-tools.meta.js
@@ -2,7 +2,7 @@
 // @author         breunigs
 // @name           IITC plugin: Draw tools
 // @category       Draw
-// @version        0.10.2.20240318.183126
+// @version        0.10.2.20240318.184317
 // @description    Allow drawing things onto the current map so you may plan your next move. Supports Multi-Project-Extension.
 // @id             draw-tools
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR711/plugins/links-to-moved-portals.user.js
+++ build/artifact/PR711/plugins/links-to-moved-portals.user.js
@@ -2,7 +2,7 @@
 // @author         screach
 // @name           IITC plugin: Links to moved portals
 // @category       Layer
-// @version        0.1.0.20240318.183126
+// @version        0.1.0.20240318.184317
 // @description    Show links to portals with different location data
 // @id             links-to-moved-portals
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -22,7 +22,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2024-03-18-183126';
+plugin_info.dateTimeVersion = '2024-03-18-184317';
 plugin_info.pluginId = 'links-to-moved-portals';
 //END PLUGIN AUTHORS NOTE
 
