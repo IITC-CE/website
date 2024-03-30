8e32b408847e91fef016d32cadea024796a17771
IITC-CE <carles3@pina.cat>
2024-03-30 21:28:15 +0000

    Update from https://github.com/IITC-CE/website/commit/6be9d885cf88fa4e40713babc4748ef1d0bbb532

---
+++ build/artifact/PR723/plugins/portal-names.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         ZasoGD
+// @name           IITC plugin: Portal Names
+// @category       Layer
+// @version        0.2.2.20240330.212200
+// @description    Show portal names on the map.
+// @id             portal-names
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR723/plugins/portal-names.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR723/plugins/portal-names.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/portal-names.png
+// @icon64         https://iitc.app/extras/plugin-icons/portal-names-64.png
+// @grant          none
+// ==/UserScript==
