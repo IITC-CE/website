1acd8e9f9f53d7a922842ee85c4ba8782b13a65d
Alexander Danilov <git@modos189.ru>
2024-07-12 22:26:14 +0500

    Added a category with recently updated plugins

--- pages/community_plugins.py
+++ pages/community_plugins.py
@@ -6,6 +6,7 @@ import os
 import requests
 import json
 import logging
+from datetime import datetime
 from typing import List, Dict
 from operator import itemgetter
 
@@ -36,6 +37,17 @@ def get_community_plugins() -> List[Dict]:
         return []
 
 
+def get_latest_entries(plugins: List[Dict], data_key: str) -> List[Dict]:
+    def parse_date(date_str):
+        # Remove 'Z' suffix and parse the date string
+        if date_str.endswith('Z'):
+            date_str = date_str[:-1]
+        return datetime.fromisoformat(date_str)
+
+    sorted_list = sorted(plugins, key=lambda x: parse_date(x[data_key]), reverse=True)
+    return sorted_list[:6]
+
+
 def sort_plugins_by_name(plugins: List[Dict]) -> List[Dict]:
     return sorted(plugins, key=itemgetter('name'))
 
@@ -57,7 +69,14 @@ def get_community_plugins_by_categories() -> Dict[str, Dict]:
     for category in categorized.values():
         category["plugins"] = sort_plugins_by_name(category["plugins"])
 
+    sorted_categories = {
+        "last_updated": {
+            "name": "Last updated plugins",
+            "is_service": True,
+            "plugins": get_latest_entries(plugins, "updatedAt")
+        }
+    }
     # Sort categories alphabetically by category name
-    sorted_categories = dict(sorted(categorized.items()))
+    sorted_categories.update(dict(sorted(categorized.items())))
 
     return {"categories": sorted_categories}
