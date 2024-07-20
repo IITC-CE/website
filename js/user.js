9fbd6ce27af96ff81875f2d487fbe0bf4f5a8a07
IITC-CE <carles3@pina.cat>
2024-07-20 11:28:07 +0000

    Update from https://github.com/IITC-CE/website/commit/d2ae3bd6bccf08f574faa3ff04d7ee30726422d7

--- js/user.js
+++ js/user.js
@@ -3,19 +3,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     M.Sidenav.init(document.querySelectorAll('.sidenav'), {});
     M.Collapsible.init(document.querySelectorAll('.collapsible'), {});
-    M.Modal.init(document.querySelectorAll('.modal'), {});
+    M.Modal.init(document.querySelectorAll('.modal'), {onCloseStart: fixModalScroll});
     M.Tabs.init(document.querySelectorAll('.tabs'), {});
+    M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {constrainWidth: false});
     img_lazy();
 
-    if (window.location.hash.endsWith("_beta")) {
-        document.getElementById('tab_beta').click();
-    }
-
-    // Open modal with comment if hash is set
-    let plugin = $_GET("plugin");
-    if (plugin && (plugin.endsWith("_beta"))) {
-        document.getElementById(plugin).querySelectorAll('.card-plugin-info')[0].click();
-        history.pushState({}, document.title, window.location.href.replace(window.location.search, ""));
+    const hash = window.location.hash;
+    if (hash) {
+        document.querySelector(hash)?.querySelector('.plugin-info').click();
     }
 
     if (document.getElementsByClassName('remark42__counter').length) {
@@ -27,7 +22,59 @@ document.addEventListener('DOMContentLoaded', function() {
         };
         init_remark(window.remark_config);
     }
+    fixSidenavIndexHref();
+    initPluginsView();
 });
+
+function initPluginsView() {
+    const view_buttons = document.querySelector('.section_title .title_buttons .view');
+    if (view_buttons) {
+        view_buttons.classList.add("show");
+        const view_mode = localStorage.getItem("plugins_view_mode") || "grid";
+        setPluginsViewMode(view_mode, false);
+    }
+}
+
+function setPluginsViewMode(view_mode, is_save=true) {
+    if (view_mode === "grid") {
+        document.querySelector('.section_title .title_buttons .view .grid').classList.remove("lite");
+        document.querySelector('.section_title .title_buttons .view .list').classList.add("lite");
+    } else {
+        document.querySelector('.section_title .title_buttons .view .grid').classList.add("lite");
+        document.querySelector('.section_title .title_buttons .view .list').classList.remove("lite");
+    }
+    document.querySelector('body').classList.remove("plugins_view_grid");
+    document.querySelector('body').classList.remove("plugins_view_list");
+    document.querySelector('body').classList.add("plugins_view_" + view_mode);
+    if (is_save) {
+        localStorage.setItem("plugins_view_mode", view_mode);
+    }
+}
+
+// Fix homepage link if the site is running on GitHub Pages or locally
+function fixSidenavIndexHref() {
+  const currentPath = window.location.pathname;
+  const pathParts = currentPath.split('/');
+
+  // Check if the page is not in the root directory
+  // pathParts[0] will be an empty string, pathParts[1] will be the filename or folder
+  if (pathParts.length > 2) {
+    const sidenavIndexLink = document.querySelector('.sidenav .index a');
+    if (sidenavIndexLink) {
+      sidenavIndexLink.setAttribute('href', 'index.html');
+    }
+  }
+}
+
+function fixModalScroll() {
+    document.querySelector("html").style.scrollBehavior = "auto";
+    setTimeout(
+        () => {
+            document.querySelector("html").style.scrollBehavior = "smooth";
+        },
+        250
+    )
+}
 
 // Closing modal of plugin by pressing back button
 window.addEventListener('popstate', function(event) {
@@ -42,8 +89,8 @@ function $_GET(key) {
     return p ? p[1] : false;
 }
 
-function modal_move_content(content_id, modal_id) {
-    document.getElementById(modal_id).innerHTML = document.getElementById(content_id).innerHTML;
+function modal_move_content(plugin_info, modal_id) {
+    document.getElementById(modal_id).innerHTML = plugin_info.innerHTML;
 }
 
 function init_modal_remark(plugin, el, url) {
@@ -61,11 +108,26 @@ function init_modal_remark(plugin, el, url) {
 }
 
 function init_custom_modal(plugin_unique_id, modal_id, el, url) {
-    window.location.hash = el.parentNode.id;
-    modal_move_content(plugin_unique_id+"_info", modal_id);
+    window.location.hash = el.id;
+    const plugin_info = document.getElementById(plugin_unique_id+"_info");
+    modal_move_content(plugin_info, modal_id);
+
+    const modal_footer = document.getElementById(modal_id).parentNode.parentNode.querySelector(".modal-footer");
+
+    set_modal_data(modal_footer, ".install-plugin", plugin_info.dataset.download);
+    set_modal_data(modal_footer, ".install-plugin-release", plugin_info.dataset.downloadRelease);
+    set_modal_data(modal_footer, ".install-plugin-beta", plugin_info.dataset.downloadBeta);
+
+    set_modal_data(modal_footer, ".homepage-plugin", plugin_info.dataset.homepage);
+    set_modal_data(modal_footer, ".issue-tracker-plugin", plugin_info.dataset.issueTracker);
 
     document.getElementById("remark42").innerHTML = "";
     init_modal_remark(plugin_unique_id, el, url);
+}
+
+function set_modal_data(modal_footer, btn_class, href) {
+    modal_footer.querySelector(btn_class).href = (href) ? href : "#";
+    modal_footer.querySelector(btn_class).style.display = (href) ? "inline-flex" : "none";
 }
 
 function init_remark(remark_config) {
