f4127088d280dea54b666c5c74e40fc87685fba1
IITC-CE <carles3@pina.cat>
2024-08-09 15:17:06 +0000

    Update from https://github.com/IITC-CE/website/commit/8413050c5511b4cf405e15e47a083e9965a3cecc

---
+++ build/artifact/PR751/plugins/basemap-google-gray.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         jacob1123
+// @name           IITC plugin: Gray Google map
+// @category       Map Tiles
+// @version        0.1.5.20240809.150540
+// @description    Add a simplified gray Version of Google map tiles as an optional layer.
+// @id             basemap-google-gray
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR751/plugins/basemap-google-gray.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR751/plugins/basemap-google-gray.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/basemap-google-gray.png
+// @icon64         https://iitc.app/extras/plugin-icons/basemap-google-gray-64.png
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR751/plugins/links-to-moved-portals.meta.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @author         screach
+// @name           IITC plugin: Links to moved portals
+// @category       Layer
+// @version        0.1.0.20240809.150540
+// @description    Show links to portals with different location data
+// @id             links-to-moved-portals
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR751/plugins/links-to-moved-portals.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR751/plugins/links-to-moved-portals.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/links-to-moved-portals.png
+// @icon64         https://iitc.app/extras/plugin-icons/links-to-moved-portals-64.png
+// @grant          none
+// ==/UserScript==
