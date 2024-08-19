7779e878c2495fb03789842a05a977a1aa6fd1b3
IITC-CE <carles3@pina.cat>
2024-08-19 16:58:35 +0000

    Update from https://github.com/IITC-CE/website/commit/bfa77596e9be08fdb5f26f617b2c6c7a31f6725c

---
+++ build/artifact/PR751/plugins/player-activity-tracker.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         breunigs
+// @name           IITC plugin: Player activity tracker
+// @category       Layer
+// @version        0.13.0.20240819.165332
+// @description    Draw trails for the path a user took onto the map based on status messages in COMMs. Uses up to three hours of data. Does not request chat data on its own, even if that would be useful.
+// @id             player-activity-tracker
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR751/plugins/player-activity-tracker.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR751/plugins/player-activity-tracker.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/player-activity-tracker.svg
+// @grant          none
+// ==/UserScript==
