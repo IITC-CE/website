48840d41ff9f9d9db120f1f6f9ef49ea94eb2dd9
IITC-CE <carles3@pina.cat>
2024-09-17 19:17:30 +0000

    Update from https://github.com/IITC-CE/website/commit/f74665c241a4a9e2f55613e18a44514355ddb42e

---
+++ build/artifact/PR760/plugins/layer-count.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         fkloft
+// @name           IITC plugin: Layer count
+// @category       Info
+// @version        0.2.3.20240917.191202
+// @description    Allow users to count nested fields
+// @id             layer-count
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR760/plugins/layer-count.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR760/plugins/layer-count.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/layer-count.svg
+// @grant          none
+// ==/UserScript==
