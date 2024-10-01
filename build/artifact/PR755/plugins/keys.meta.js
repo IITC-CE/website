c01831d6f2832e09993d0dbaf21b8b66c8835793
IITC-CE <carles3@pina.cat>
2024-10-01 09:58:55 +0000

    Update from https://github.com/IITC-CE/website/commit/6388b50a5b085770f36b6bb2cd96060582a75f51

---
+++ build/artifact/PR755/plugins/keys.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         xelio
+// @name           IITC plugin: Keys
+// @category       Misc
+// @version        0.4.2.20241001.095400
+// @description    Allow manual entry of key counts for each portal. Use the 'keys-on-map' plugin to show the numbers on the map, and 'sync' to share between multiple browsers or desktop/mobile.
+// @id             keys
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR755/plugins/keys.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR755/plugins/keys.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/keys.svg
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR755/plugins/regions.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Ingress scoring regions
+// @category       Layer
+// @version        0.3.2.20241001.095400
+// @description    Show the regional scoring cells grid on the map
+// @id             regions
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR755/plugins/regions.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR755/plugins/regions.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/regions.svg
+// @grant          none
+// ==/UserScript==
