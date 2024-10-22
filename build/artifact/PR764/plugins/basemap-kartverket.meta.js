30eed03823d6c640a33814293732e84444d850ca
IITC-CE <carles3@pina.cat>
2024-10-22 07:11:07 +0000

    Update from https://github.com/IITC-CE/website/commit/a76bece8c487b15185dfe7166bb9d1faa1e94da4

---
+++ build/artifact/PR764/plugins/basemap-kartverket.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Kartverket.no maps (Norway)
+// @category       Map Tiles
+// @version        0.3.0.20241022.070627
+// @description    Add Kartverket.no map layers.
+// @id             basemap-kartverket
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR764/plugins/basemap-kartverket.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR764/plugins/basemap-kartverket.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/basemap-kartverket.svg
+// @grant          none
+// ==/UserScript==
