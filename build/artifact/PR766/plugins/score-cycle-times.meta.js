be246ea62f477790438fae9354dccb86a506bbfa
IITC-CE <carles3@pina.cat>
2024-10-24 08:58:10 +0000

    Update from https://github.com/IITC-CE/website/commit/ad1a2b4df5637a2c3610291f6dc2da0b7ab06cf3

---
+++ build/artifact/PR766/plugins/score-cycle-times.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Scoring cycle / checkpoint times
+// @category       Info
+// @version        0.2.2.20241024.085237
+// @description    Show the times used for the septicycle and checkpoints for regional scoreboards.
+// @id             score-cycle-times
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR766/plugins/score-cycle-times.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR766/plugins/score-cycle-times.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/score-cycle-times.svg
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR766/plugins/zoom-slider.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         fragger
+// @name           IITC plugin: Zoom slider
+// @category       Controls
+// @version        0.2.3.20241024.085237
+// @description    Show a zoom slider on the map instead of the zoom buttons.
+// @id             zoom-slider
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR766/plugins/zoom-slider.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR766/plugins/zoom-slider.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/zoom-slider.svg
+// @grant          none
+// ==/UserScript==
