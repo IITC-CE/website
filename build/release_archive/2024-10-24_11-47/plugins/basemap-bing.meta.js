d90f9c0ffbf79431685ea2909ac5aaaf4f272595
IITC-CE <carles3@pina.cat>
2024-10-24 11:54:16 +0000

    Update from https://github.com/IITC-CE/website/commit/5fb9aa95af2c7a6d4deb86017f20bf5593c25402

---
+++ build/release_archive/2024-10-24_11-47/plugins/basemap-bing.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Bing maps
+// @category       Map Tiles
+// @version        0.3.3
+// @description    Add the bing.com map layers.
+// @id             basemap-bing
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/release/plugins/basemap-bing.meta.js
+// @downloadURL    https://iitc.app/build/release/plugins/basemap-bing.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/basemap-bing.svg
+// @grant          none
+// ==/UserScript==
