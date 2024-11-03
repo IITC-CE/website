eb8dc7c07d8c0374ce14bd628bb50ca03000e2c0
IITC-CE <carles3@pina.cat>
2024-11-03 15:34:55 +0000

    Update from https://github.com/IITC-CE/website/commit/9424c0c54fcb94cf925259a347da876f4848dba0

---
+++ build/artifact/PR779/plugins/farms-find.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         949
+// @name           IITC plugin: Find farms on map
+// @category       Layer
+// @version        1.4.5.20241103.152915
+// @description    Show farms by minimum level
+// @id             farms-find
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR779/plugins/farms-find.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR779/plugins/farms-find.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/farms-find.svg
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR779/plugins/machina-tracker.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name           IITC plugin: Machina tracker
+// @author         McBen
+// @category       Layer
+// @version        1.0.1.20241103.152915
+// @description    Show locations of Machina activities
+// @id             machina-tracker
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR779/plugins/machina-tracker.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR779/plugins/machina-tracker.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/machina-tracker.svg
+// @grant          none
+// ==/UserScript==
