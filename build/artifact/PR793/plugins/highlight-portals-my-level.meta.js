6050361844b0c728881fdc938af7dfad11fbc794
IITC-CE <carles3@pina.cat>
2024-12-19 22:14:19 +0000

    Update from https://github.com/IITC-CE/website/commit/7e275559226efa87d6be8ec13e7663922aefd2d5

---
+++ build/artifact/PR793/plugins/highlight-portals-my-level.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         vita10gy
+// @name           IITC plugin: Highlight portals by my level
+// @category       Highlighter
+// @version        0.2.3.20241219.220853
+// @description    Use the portal fill color to denote if the portal is either at and above, or at and below your level.
+// @id             highlight-portals-my-level
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR793/plugins/highlight-portals-my-level.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR793/plugins/highlight-portals-my-level.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/highlight-portals-my-level.svg
+// @grant          none
+// ==/UserScript==
