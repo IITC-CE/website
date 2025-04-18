ddda19e7508b7cb74e8bcd1e697977b0a8dbec05
IITC-CE <carles3@pina.cat>
2025-04-18 08:21:08 +0000

    Update from https://github.com/IITC-CE/website/commit/06921699a25eb207313dae7d3860776978b47224

---
+++ build/artifact/PR809/plugins/overlay-kml.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         danielatkins
+// @name           IITC plugin: Overlay KML / GPX / GeoJSON
+// @category       Layer
+// @version        0.3.3.20250418.081514
+// @description    Allow users to overlay their own KML / GPX / GeoJSON files on top of IITC.
+// @id             overlay-kml
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR809/plugins/overlay-kml.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR809/plugins/overlay-kml.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/overlay-kml.svg
+// @grant          none
+// ==/UserScript==
