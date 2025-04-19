05cd654273b9ff0068497237cabff54f115ed965
IITC-CE <carles3@pina.cat>
2025-04-19 12:26:56 +0000

    Update from https://github.com/IITC-CE/website/commit/06c9c6ffe600152eece8beb1f2e039326cc0d41c

---
+++ build/artifact/PR812/plugins/basemap-blank.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         jonatkins
+// @name           IITC plugin: Blank map
+// @category       Map Tiles
+// @version        0.1.6.20250419.121923
+// @description    Add a blank map layer - no roads or other features.
+// @id             basemap-blank
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR812/plugins/basemap-blank.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR812/plugins/basemap-blank.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/basemap-blank.svg
+// @grant          none
+// ==/UserScript==
