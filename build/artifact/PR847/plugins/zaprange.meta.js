4d9253c7d28495e9a4dd13f07a0d067746460351
IITC-CE <carles3@pina.cat>
2025-07-30 14:38:01 +0000

    Update from https://github.com/IITC-CE/website/commit/b49c99c7d09fdd884e7c891ff6b108adb8b6ee15

---
+++ build/artifact/PR847/plugins/zaprange.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         ZasoGD
+// @name           IITC plugin: Zaprange
+// @category       Layer
+// @version        0.1.8.20250730.143155
+// @description    Shows the maximum range of attack by the portals.
+// @id             zaprange
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR847/plugins/zaprange.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR847/plugins/zaprange.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/zaprange.svg
+// @grant          none
+// ==/UserScript==
