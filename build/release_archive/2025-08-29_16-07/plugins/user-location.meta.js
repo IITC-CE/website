1b123765901b8fdca9e143e1a43fb25fcc375803
IITC-CE <carles3@pina.cat>
2025-08-29 16:17:13 +0000

    Update from https://github.com/IITC-CE/website/commit/3d554a39e5da932609676b5e50f2a9c5bfce65ba

---
+++ build/release_archive/2025-08-29_16-07/plugins/user-location.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         cradle
+// @name           IITC plugin: User Location
+// @category       Tweaks
+// @version        0.3.0
+// @description    Show user location marker on map
+// @id             user-location
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/release/plugins/user-location.meta.js
+// @downloadURL    https://iitc.app/build/release/plugins/user-location.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/user-location.svg
+// @grant          none
+// ==/UserScript==
