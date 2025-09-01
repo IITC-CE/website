a05ec893d7908ae43b05a4222b9f34f96087cb08
IITC-CE <carles3@pina.cat>
2025-09-01 15:47:27 +0000

    Update from https://github.com/IITC-CE/website/commit/ff3a39b1b324be4ce2adfabfff71cae1e9c08194

---
+++ build/artifact/PR860/plugins/linked-portals-show.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         fstopienski
+// @name           IITC plugin: Linked portals
+// @category       Portal Info
+// @version        0.4.3.20250901.154149
+// @description    Try to show the linked portals (image, name and link direction) in portal detail view and jump to linked portal on click.  Some details may not be available if the linked portal is not in the current view.
+// @id             linked-portals-show
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR860/plugins/linked-portals-show.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR860/plugins/linked-portals-show.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/linked-portals-show.svg
+// @grant          none
+// ==/UserScript==
