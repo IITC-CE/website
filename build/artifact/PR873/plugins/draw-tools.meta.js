fd36376899e79b1b2d9db55b96fadb6dd3813cb6
IITC-CE <carles3@pina.cat>
2025-10-07 10:16:05 +0000

    Update from https://github.com/IITC-CE/website/commit/255d924a5e30b94eb44b7e1888c1811e314b85d8

---
+++ build/artifact/PR873/plugins/draw-tools.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         breunigs
+// @name           IITC plugin: Draw tools
+// @category       Draw
+// @version        0.12.0.20251007.100610
+// @description    Allow drawing things onto the current map so you may plan your next move. Supports Multi-Project-Extension.
+// @id             draw-tools
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR873/plugins/draw-tools.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR873/plugins/draw-tools.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/draw-tools.svg
+// @grant          none
+// ==/UserScript==
---
+++ build/artifact/PR873/plugins/fix-china-map-offset.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         modos189
+// @name           IITC plugin: Fix maps offsets in China
+// @category       Tweaks
+// @version        0.3.4.20251007.100610
+// @description    Show correct maps for China user by applying offset tweaks.
+// @id             fix-china-map-offset
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR873/plugins/fix-china-map-offset.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR873/plugins/fix-china-map-offset.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/fix-china-map-offset.svg
+// @grant          none
+// ==/UserScript==
