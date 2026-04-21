7287c6cea6ff3e5831c40e9d10a86eed27c5eea9
IITC-CE <carles3@pina.cat>
2026-04-21 16:22:09 +0000

    Update from https://github.com/IITC-CE/website/commit/d1adedc2d79fd2679226433dae7795a966fa4f12

---
+++ build/artifact/PR903/plugins/minimap.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @author         johnd0e
+// @name           IITC plugin: Mini map
+// @category       Controls
+// @version        0.4.5.20260421.161514
+// @description    Show a mini map on the corner of the map.
+// @id             minimap
+// @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
+// @updateURL      https://iitc.app/build/artifact/PR903/plugins/minimap.meta.js
+// @downloadURL    https://iitc.app/build/artifact/PR903/plugins/minimap.user.js
+// @match          https://intel.ingress.com/*
+// @match          https://intel-x.ingress.com/*
+// @icon           https://iitc.app/extras/plugin-icons/minimap.svg
+// @grant          none
+// ==/UserScript==
