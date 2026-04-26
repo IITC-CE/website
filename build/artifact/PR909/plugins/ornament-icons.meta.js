028a395959f913d84b8580d757cdb308ad5b0d5f
IITC-CE <carles3@pina.cat>
2026-04-26 18:34:23 +0000

    Update from https://github.com/IITC-CE/website/commit/0978da2231afcd4736d61e758e4d09cd4e2d18de

--- build/artifact/PR909/plugins/ornament-icons.meta.js
+++ build/artifact/PR909/plugins/ornament-icons.meta.js
@@ -2,7 +2,7 @@
 // @author         johtata
 // @name           IITC plugin: Ornament icons basic
 // @category       Layer
-// @version        0.1.3.20260426.182003
+// @version        0.1.3.20260426.182857
 // @description    Add own icons and names for ornaments
 // @id             ornament-icons
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR909/plugins/portal-counts.user.js
+++ build/artifact/PR909/plugins/portal-counts.user.js
@@ -2,7 +2,7 @@
 // @author         yenky
 // @name           IITC plugin: Portal count
 // @category       Info
-// @version        0.2.6.20260426.182003
+// @version        0.2.6.20260426.182857
 // @description    Display a list of all localized portals by level and faction.
 // @id             portal-counts
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
@@ -21,7 +21,7 @@ if(typeof window.plugin !== 'function') window.plugin = function() {};
 //PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
 //(leaving them in place might break the 'About IITC' page or break update checks)
 plugin_info.buildName = 'test';
-plugin_info.dateTimeVersion = '2026-04-26-182003';
+plugin_info.dateTimeVersion = '2026-04-26-182857';
 plugin_info.pluginId = 'portal-counts';
 //END PLUGIN AUTHORS NOTE
 
