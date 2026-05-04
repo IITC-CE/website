cd1c3a72825eb43442a3c1d1371c33f4fc7e4d02
IITC-CE <carles3@pina.cat>
2026-05-04 11:51:04 +0000

    Update from https://github.com/IITC-CE/website/commit/6a0286b079726edd4becd4ec7ba00b58e0947608

--- build/artifact/PR916/plugins/basemap-openstreetmap.meta.js
+++ build/artifact/PR916/plugins/basemap-openstreetmap.meta.js
@@ -2,7 +2,7 @@
 // @author         jonatkins
 // @name           IITC plugin: OpenStreetMap.org map
 // @category       Map Tiles
-// @version        0.1.6.20260504.114244
+// @version        0.1.6.20260504.114541
 // @description    Add the native OpenStreetMap.org map tiles as an optional layer.
 // @id             basemap-openstreetmap
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
