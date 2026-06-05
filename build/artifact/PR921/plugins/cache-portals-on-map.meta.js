ffcf9dadc78db8cf988871b11d6c788feb6fe092
IITC-CE <carles3@pina.cat>
2026-06-05 07:26:14 +0000

    Update from https://github.com/IITC-CE/website/commit/10cd9603876a08fdbfb94cc24e0af57759720176

--- build/artifact/PR921/plugins/cache-portals-on-map.meta.js
+++ build/artifact/PR921/plugins/cache-portals-on-map.meta.js
@@ -2,7 +2,7 @@
 // @author         jonatkins
 // @name           IITC plugin: Cache viewed portals on map
 // @category       Cache
-// @version        0.1.3.20260604.131844
+// @version        0.1.3.20260605.072058
 // @description    Cache the details of recently viewed portals and use this to populate the map when possible
 // @id             cache-portals-on-map
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR921/plugins/highlight-moved-portals.meta.js
+++ build/artifact/PR921/plugins/highlight-moved-portals.meta.js
@@ -2,7 +2,7 @@
 // @author         screach
 // @name           IITC plugin: Highlight moved portals
 // @category       Highlighter
-// @version        0.1.2.20260604.131844
+// @version        0.1.2.20260605.072058
 // @description    Highlights portals with links with different location data
 // @id             highlight-moved-portals
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
