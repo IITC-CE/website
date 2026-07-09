e147003aeb9d885eddbfeefee30715746ee78fc5
IITC-CE <carles3@pina.cat>
2026-07-09 02:41:38 +0000

    Update from https://github.com/IITC-CE/website/commit/f24921a577ffc58383b305a6880135c2ce47b452

--- build/artifact/PR937/plugins/highlight-portal-history.meta.js
+++ build/artifact/PR937/plugins/highlight-portal-history.meta.js
@@ -2,7 +2,7 @@
 // @author         Johtaja
 // @name           IITC plugin: Highlight portals based on history
 // @category       Highlighter
-// @version        0.3.3.20260709.022849
+// @version        0.3.3.20260709.023603
 // @description    Use the portal fill color to denote the portal has been visited, captured, scout controlled
 // @id             highlight-portal-history
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
