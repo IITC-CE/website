e35ade4705344d95ca74753852cce586f7c8a0b0
IITC-CE <carles3@pina.cat>
2026-09-07 08:49:25 +0000

    Update from https://github.com/IITC-CE/website/commit/1f9182c576cdd2f21fa66c00039076203cbc9432

--- build/artifact/PR961/plugins/pan-control.meta.js
+++ build/artifact/PR961/plugins/pan-control.meta.js
@@ -2,7 +2,7 @@
 // @author         fragger
 // @name           IITC plugin: Pan control
 // @category       Controls
-// @version        0.2.5.20260904.140744
+// @version        0.2.5.20260907.084359
 // @description    Show a panning control on the map.
 // @id             pan-control
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR961/plugins/scoreboard.meta.js
+++ build/artifact/PR961/plugins/scoreboard.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @author         Costaspap
 // @name           IITC plugin: Localized scoreboard
-// @version        0.4.1.20260904.140744
+// @version        0.4.1.20260907.084359
 // @category       Info
 // @description    Display a scoreboard about all visible portals with statistics about both teams,like average portal level,link & field counts etc.
 // @id             scoreboard
