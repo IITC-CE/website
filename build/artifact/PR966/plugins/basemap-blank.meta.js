ce20057e00fc27c3a8530bd91bd5bfff6ead84db
IITC-CE <carles3@pina.cat>
2026-09-16 14:51:58 +0000

    Update from https://github.com/IITC-CE/website/commit/e31b8f23b4a3ac05a085ebeadba387d3bcc2c493

--- build/artifact/PR966/plugins/basemap-blank.meta.js
+++ build/artifact/PR966/plugins/basemap-blank.meta.js
@@ -2,7 +2,7 @@
 // @author         jonatkins
 // @name           IITC plugin: Blank map
 // @category       Map Tiles
-// @version        0.1.6.20260916.123008
+// @version        0.1.6.20260916.144523
 // @description    Add a blank map layer - no roads or other features.
 // @id             basemap-blank
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
--- build/artifact/PR966/plugins/highlight-portal-history.meta.js
+++ build/artifact/PR966/plugins/highlight-portal-history.meta.js
@@ -2,7 +2,7 @@
 // @author         Johtaja
 // @name           IITC plugin: Highlight portals based on history
 // @category       Highlighter
-// @version        0.3.3.20260916.123008
+// @version        0.3.3.20260916.144523
 // @description    Use the portal fill color to denote the portal has been visited, captured, scout controlled
 // @id             highlight-portal-history
 // @namespace      https://github.com/IITC-CE/ingress-intel-total-conversion
